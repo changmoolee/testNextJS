232011e8d9e850544e96a50e5bd06f439157314f
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-10-13 22:45:34 +0900

    css - styled-jsx 문법 및 _app.js로 모든 컴포넌트 다루기

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import NavBar from "../components/NavBar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <span>hello</span>
+      <style jsx global>
+        {`
+          a {
+            color: white;
+          }
+        `}
+      </style>
+    </>
+  );
+}
