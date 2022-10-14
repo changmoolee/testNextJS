a8b966c003f5a0fc1f0207472e70724d6b9e1f2e
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-10-14 19:25:47 +0900

    Feat: redirect - 유저 특정 url을 다른 url로 리다이렉트/ rewrite - url 요청 특정 url로 고쳐서

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*", // 유저가 여기로 이동하면
+        destination: "/new-blog/:path*", // 여기로 Redirect 시켜줄 것이다
+        permanent: false,
+      },
+      {
+        source: "/old-phone/:path*", // 유저가 여기로 이동하면
+        destination: "/new-phone/:path*", // 여기로 Redirect 시켜줄 것이다
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
